0c5565cd86f7f563550a7bd99dc3e6b7cee853c2
amazemkhonto <amaze.mkhonto2@gmail.com>
2023-06-12 22:27:07 +0200

    practice

--- Practice/Practice.cs
+++ Practice/Practice.cs
@@ -1,91 +1,72 @@
-﻿// System.Console.WriteLine("How many points did the student get");
-
-// string userInput = Console.ReadLine();
-
-// int points = int.Parse(userInput);
-
-// char grade;
-
-// switch (points){
-//     case 9:
-//     case 10:
-//         grade = 'A';
-//         break;
-//     case 8:
-//     case 7:
-//     case 6: 
-//         grade = 'B';
-//         break;
-//     case 5:
-//     case 4:
-//     case 3:
-//         grade = 'C';
-//         break;
-//     case 2:
-//     case 1:
-//         grade = 'D';
-//         break;
-//     default:
-//         grade = '!';
-//         break;
-// }
-
-// System.Console.WriteLine("Your grade is: "+ grade);
-
-
-// while loop
-
-// int number = 0;
-// while (number < 10){
-//     System.Console.WriteLine("The number is less than 10: "+number);
-//     //number = number + 1;
-//     //number += 1;
-//     number ++; // this one is used when you want to increment by 1
-//     }
-// System.Console.WriteLine("The loop");
+﻿
 
 /*
-
-While loop - CalculateSumOfNumbersBetween
-
-Using the while loop, implement the CalculateSumOfNumbersBetween method, 
-which given two integers called firstNumber and lastNumber, should calculate 
-the sum of numbers between them (including the numbers themselves).
-
-
-If lastNumber is smaller than firstNumber, the result shall be 0.
-
+For loop - Factorial
+Using the for loop, implement the Factorial method, which calculates the factorial of a given number.
+Factorial of number N is the result of multiplying all integers from 1 to N.
+N! = 1 * 2 * 3 * ... * (N-1) * N
 For example:
+    5! = 1*2*3*4*5 = 120
+    4! = 1*2*3*4 = 24
+    1! = 1
+    0! = 1 (by convention)
+Assume the number parameter is non-negative.*/
 
-    for firstNumber equal to 5 and lastNumber equal to 10, the result should be 45
-     because this is the sum of numbers 5, 6, 7, 8, 9, 10
+// int number = 0;
+// int factorial =  1;
+// for(int i = 1; i < number+1; i++){
+//     factorial = factorial *i;
+// }
+// System.Console.WriteLine(factorial);
 
-    for firstNumber equal to -5 and lastNumber equal to 5, the result should be 0 
-    because this is the sum of numbers -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5
+//use a do while look and break, to ask a user to enter a number larger than 10 if they type in stop
+// you should stop the loop (also is the do wile loop the best one to use)
 
-    for firstNumber equal to 10 and lastNumber also equal to 10, the result should be 10 
-    because, to the sum, we include the only number, which is 10
-
-    for firstNumber equal to 5 and lastNumber equal to 4, the result should be 0 because 
-    the lastNumber is smaller than firstNumber
-
-*/
-// int CalculateSumOfNumbersBetween(int firstNumber, int lastNumber){
+// int enteredNumber;
+// do{
+//     System.Console.WriteLine("Enter a number larger than 10: ");
+//     string userInput = Console.ReadLine();
     
+//     if(userInput == "stop"){
+//         break;
 //     }
+//     enteredNumber =  int.Parse(userInput);
+// } while (enteredNumber < 10);
 
 
-int x = 5;
-int y = 10;
+// given numbers print all the numbers in the console unless it is divisible by 3, use continue
+// int n = 10;
+// for(int i = 1; i< n; i++){
+//     if(i%3==0){
+//         continue;
+//     }
+//     System.Console.WriteLine(i);
+// }
+// continue is used to skip while break is used to terminate the loop
+// calculate the sum of all arrays
 
-int z = x;
+// int[] numbers = {1,2,3,4,5};
 
-int sum = 0;
+// int sum = 0;
 
-while (z <= y){
-    sum += z;
-    z++;
-    System.Console.WriteLine(z);
-}
+// for(int i = 0; i <= numbers.Length ; i++){
+//     sum = sum + i;
+// }
 
-System.Console.WriteLine(sum);
+// System.Console.WriteLine(sum);
+
+//TODO : find a max value from a 1d array
+
+// multi-dimensional arrays declare a 2d array 3 by 3
+
+int[,] grid = new int[3,3]; // this is when you dont know the values
+
+int[,] grid2 = {{1, 2, 3}, {1, 5, 5}, {4, 5, 1}}; // declaring and initializing a 2d array og size 3 by 3
+
+// given a 3 by 2 array find the maximum value 
+
+int[,] numbers = {{3,5}, {-1,12}, {4,0}};
+
+int height = numbers.GetLength(0);
+int width = numbers.GetLength(1);
+
