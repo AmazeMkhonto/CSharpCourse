dd0ceb4808c249fe8d2ee5e27f55fe5266d219e6
Amaze Mkhonto <amaze@bbd.co.za>
2023-07-10 13:51:21 +0200

    its been a while

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,72 +1,72 @@
 ﻿
 
-Console.WriteLine("Hello!");
-Console.WriteLine("Input first number: ");
-string number1 = Console.ReadLine();
-int number1_ = int.Parse(number1);
+// Console.WriteLine("Hello!");
+// Console.WriteLine("Input first number: ");
+// string number1 = Console.ReadLine();
+// int number1_ = int.Parse(number1);
 
-Console.WriteLine("Input second number: ");
-string number2 = Console.ReadLine();
-int number2_ = int.Parse(number2);
+// Console.WriteLine("Input second number: ");
+// string number2 = Console.ReadLine();
+// int number2_ = int.Parse(number2);
 
-Console.WriteLine("What do you want to do with those numbers?");
-Console.WriteLine("[A]dd");
-Console.WriteLine("[S]ubtract");
-Console.WriteLine("[M]ultiply");
+// Console.WriteLine("What do you want to do with those numbers?");
+// Console.WriteLine("[A]dd");
+// Console.WriteLine("[S]ubtract");
+// Console.WriteLine("[M]ultiply");
 
-string opertor = Console.ReadLine();
+// string opertor = Console.ReadLine();
 
-int Addition(int a, int b){
-    return a + b;
-}
-
-int Subtraction(int a, int b){
-    return a - b;
-}
-
-int Multiplication(int a, int b){
-    return a * b;
-}
-
-//you can improve this by using a switch statement
-
-// if(opertor.ToUpper() == "A"){
-//     int results = Addition(number1_, number2_);
-//     Console.WriteLine($"{number1_} + {number2_} = {results}"); 
-
-// } else if(opertor.ToUpper() == "S"){
-//     int results = Subtraction(number1_, number2_);
-//     Console.WriteLine($"{number1_} - {number2_} = {results}"); 
-
-// } else if (opertor.ToUpper() == "M"){
-//     int results = Multiplication(number1_, number2_);
-//     Console.WriteLine($"{number1_} * {number2_} = {results}"); 
-
-// } else{
-//     System.Console.WriteLine("Invalid option");
+// int Addition(int a, int b){
+//     return a + b;
 // }
 
-int result = 0;
-switch (opertor.ToUpper()){
-    case "A":
-    result = Addition(number1_, number2_);
-    Console.WriteLine($"{number1_} + {number2_} = {result}"); 
-    break;
+// int Subtraction(int a, int b){
+//     return a - b;
+// }
 
-    case "S":
-    result = Subtraction(number1_, number2_);
-    Console.WriteLine($"{number1_} - {number2_} = {result}");
-    break;
+// int Multiplication(int a, int b){
+//     return a * b;
+// }
 
-    case "M":
-    result = Multiplication(number1_, number2_);
-    Console.WriteLine($"{number1_} * {number2_} = {result}"); 
-    break;
+// //you can improve this by using a switch statement
 
-    default:
-    System.Console.WriteLine("Invalid option");
-    break;
-}
+// // if(opertor.ToUpper() == "A"){
+// //     int results = Addition(number1_, number2_);
+// //     Console.WriteLine($"{number1_} + {number2_} = {results}"); 
+
+// // } else if(opertor.ToUpper() == "S"){
+// //     int results = Subtraction(number1_, number2_);
+// //     Console.WriteLine($"{number1_} - {number2_} = {results}"); 
+
+// // } else if (opertor.ToUpper() == "M"){
+// //     int results = Multiplication(number1_, number2_);
+// //     Console.WriteLine($"{number1_} * {number2_} = {results}"); 
+
+// // } else{
+// //     System.Console.WriteLine("Invalid option");
+// // }
+
+// int result = 0;
+// switch (opertor.ToUpper()){
+//     case "A":
+//     result = Addition(number1_, number2_);
+//     Console.WriteLine($"{number1_} + {number2_} = {result}"); 
+//     break;
+
+//     case "S":
+//     result = Subtraction(number1_, number2_);
+//     Console.WriteLine($"{number1_} - {number2_} = {result}");
+//     break;
+
+//     case "M":
+//     result = Multiplication(number1_, number2_);
+//     Console.WriteLine($"{number1_} * {number2_} = {result}"); 
+//     break;
+
+//     default:
+//     System.Console.WriteLine("Invalid option");
+//     break;
+// }
 
 
 
---
+++ OOP_part2/Program.cs
@@ -0,0 +1,67 @@
+﻿Names names = new Names();
+var path = names.BuildFilePath();
+if(File.Exists(path)){
+    System.Console.WriteLine("File already exists. Loading names...");
+    names.readFromTextFile();
+
+} else {
+    System.Console.WriteLine("names file does not exist yet");
+
+    names.AddName("Claire");
+    names.AddName("This should not be a valid name");
+    names.AddName("Tony");
+    names.AddName("Amaze");
+    names.AddName("123 not you");
+
+    System.Console.WriteLine("Svaing names to the file");
+    names.WriteToTextFile();
+
+}
+
+System.Console.WriteLine(names.Format());
+
+
+class NamesValidator{
+    public bool isValidName(string name){
+        return name.Length >= 2 && name.Length < 25 && char.IsUpper(name[0]) && name.All(char.IsLetter);
+    } 
+
+
+}
+
+class Names {
+
+    private List<string> _names = new List<string>();
+
+    public void AddName(string name){
+        if(new NamesValidator().isValidName(name)){
+            _names.Add(name);
+        }
+    }
+
+
+    
+    public void readFromTextFile(){
+        var fileContents = File.ReadAllText(BuildFilePath());
+        var nameFromFile = fileContents.Split(Environment.NewLine).ToList();
+        foreach(var name in nameFromFile){
+            AddName(name);
+        }
+
+    }
+
+    public void WriteToTextFile(){
+        File.WriteAllText(BuildFilePath(), Format());
+    }
+
+    public string BuildFilePath(){
+        return "names.txt";
+    }
+
+    public string Format(){
+        return string.Join(Environment.NewLine, _names);
+
+    }
+
+
+}
